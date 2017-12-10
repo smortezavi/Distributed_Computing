6ce16d4a25a36d54aab920d4c777a7aa61dc8155
siavash mortezavi <siavashmortezavi@siavashs-MacBook-Pro.local>
2017-12-09 23:30:47 -0800

    second add

---
+++ Data/Practice_3/quiz2.py
@@ -0,0 +1,3 @@
+from pyspark import SparkContext, SparkConf
+from user_definition import *
+#Do not add additional libraries.
---
+++ Data/Practice_3/user_definition.py
@@ -0,0 +1,2 @@
+app_name = "zip_digit_count"
+file_name = "filtered_registered_business_sf.csv"
